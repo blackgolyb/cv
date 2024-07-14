c608d2eba5d8ff021fd802165e7c57ed48093569
Blackgolyb <andrejomelnickij@gmail.com>
2024-07-14 14:28:42 +0300

    feat: added auto increase tag to publish script

--- scripts/publish_target.sh
+++ scripts/publish_target.sh
@@ -6,8 +6,7 @@ set -e
 # Задаємо змінні
 TARGET_DIR="target"
 BRANCH="release"
-TAG="v$1"
-COMMIT_MESSAGE="added new version $TAG"
+TAG_PREFIX="v"
 
 
 # Перехід до теки з вашим репозиторієм
@@ -27,6 +26,7 @@ git remote add origin $(git -C $REPO_DIR config --get remote.origin.url)
 
 # Витягування гілки release
 git fetch origin $BRANCH
+git fetch --tags 
 git checkout -b $BRANCH -f origin/$BRANCH
 
 # Копіювання вмісту теки target до тимчасової теки
@@ -41,6 +41,24 @@ if git diff-index --quiet HEAD --; then
     echo "Немає змін для коміту."
     exit 0
 fi
+
+
+if [ -n "$1" ]; then
+    TAG="${TAG_PREFIX}$1"
+else
+    LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1) || echo "${TAG_PREFIX}0.0.0")
+    if [ -z "$LATEST_TAG" ]; then
+        TAG="${TAG_PREFIX}0.0.1"
+    else
+        # Збільшення версії
+        IFS='.' read -r -a VERSION_PARTS <<< "${LATEST_TAG#"$TAG_PREFIX"}"
+        VERSION_PARTS[2]=$((VERSION_PARTS[2]+1))
+        TAG="${TAG_PREFIX}${VERSION_PARTS[0]}.${VERSION_PARTS[1]}.${VERSION_PARTS[2]}"
+    fi
+fi
+
+COMMIT_MESSAGE="added new version $TAG"
+
 
 # Створення коміту з повідомленням
 git commit -m "$COMMIT_MESSAGE"
